14b80965949b5619ef251ddb76dbbf6e2b1c34aa
Abhinand KR <nanduttanvsabhi@gmail.com>
2023-10-14 10:27:18 +0530

    updated

--- sample-server/main.go
+++ sample-server/main.go
@@ -2,13 +2,11 @@ package main
 
 import "net/http"
 
-
-
 func main() {
 
 	http.HandleFunc("/",index)
 	http.ListenAndServe(":3000",nil)
-	
+
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
